028bb38f4b27379248a219e2c1d754e4e083ef4a
dnrghks5583 <dnrghks9708@naver.com>
2022-01-14 17:09:32 +0900

    Python 1일차 진행

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # StudyPython
 파이썬 학습 repository
+
+
+### 1일차
+- 프로그래밍 이해
+- Python 설치, Visual Studio Code 설치
+- 개발환경 학습
+- 기본문법
+  - Console 출력 (Hello, World!)
+  - 사칙연산
+  - type() 함수
+  - int(), float()와 같은 형변환 함수
---
+++ day1/Basic Syntax 01.py
@@ -0,0 +1,38 @@
+# 주석입니다.
+# 문자열 표현방법
+
+print('Hello, World!')
+print("Hello, World!")
+
+# 숫자 연산 출력
+# print 활용
+print(3 + 4)
+
+# 변수 사용
+a = '안녕하세요.'
+b = '반갑습니다!'
+
+print(a, b, '정말?')
+
+# 사칙연산
+print(5 +  6)
+print(5 -  6)
+print(5 *  6)
+print(5 /  3)
+print(5 // 3)
+print(5 %  3)
+print(2 **10)
+
+print(int(5 / 3))
+print(int('10'))
+# print(int('십')) 형변환 불가능
+
+print(float(10.0))
+
+# Type 확인
+print(type(10))
+print(type(10.0))
+
+# 괄호 (연산자 우선순위)
+print((2 + 3) * 5)
+print(2 + (3 * 5))
